ca5aa8d7c0d4c678227b2f3e7ddd5fe098f7231f
clOUdy <riyanshverma@jklu.edu.in>
2026-09-17 11:10:25 +0530

    chore: update resume URL

--- src/Data/personal-info.ts
+++ src/Data/personal-info.ts
@@ -7,7 +7,7 @@ const profilePictureUrl1: string = "https://res.cloudinary.com/cloudywork/image/
 const profilePictureUrl2: string = "https://res.cloudinary.com/cloudywork/image/upload/v1769173439/Me_rvuttw.jpg"
 const profilePictureUrl3: string = "https://res.cloudinary.com/cloudywork/image/upload/v1787831247/me_jfedru.png"
 const profileBannerUrl: string = "https://res.cloudinary.com/cloudywork/image/upload/v1769270261/yodo_ezlexy.png"
-const resumeUrl: string = "https://drive.google.com/file/d/1EvSux0i7vwUS5QvlBRzq3BPBbTGCZDw3/preview"
+const resumeUrl: string = "https://drive.google.com/file/d/1iBoIaMMa9dLyoLjBKW-0wU7PB71xE30q/preview"
 
 const techStack: TechBadgeProp[] = [
   { icon: SiNextdotjs, name: "Next.js", color: "text-foreground" },
